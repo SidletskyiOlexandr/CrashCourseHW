9657c5ff64d07ab3aab83343923000d7db103a80
Olexandr Sidletskyi <scasha2000@gmail.com>
2021-09-21 23:34:57 +0300

    Implement Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "This is a string with several words";
+
+            //char[] arrStr = str.ToCharArray();
+            string[] arrStr = str.Split();
+            Array.Reverse(arrStr);
+
+            foreach (var item in arrStr)
+            {
+                Console.Write(item + ' ');
+            }
+        }
+    }
+}
