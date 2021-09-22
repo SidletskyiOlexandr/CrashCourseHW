9086a909cbe3c3f9919b3857f183fbf369891c24
Olexandr Sidletskyi <scasha2000@gmail.com>
2021-09-22 14:52:46 +0300

    Implement Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = { "abcdefg", "qwerty123", "abc", "1234567" };
+            int wordLength = 7;
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].Length == wordLength)
+                {
+                    for (int j = 1; j < 4; j++)
+                    {
+                        arr[i] = arr[i].Replace(arr[i][wordLength - j], '$');
+                    }
+                }
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
