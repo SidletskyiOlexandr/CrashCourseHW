b8f471dd82cedfcbbab99ac2e4759c35a5bac35b
Olexandr Sidletskyi <scasha2000@gmail.com>
2021-09-22 22:32:35 +0300

    Implement Task9

---
+++ Task9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "This   is a  Task9     ";
+
+            str = str.Trim();
+
+            for (int i = 0; i < str.Length-1; i++)
+            {
+                if ( str[i] == ' ' && str[i+1] == ' ' )
+                {
+                    str = str.Remove(i,1);
+                    i--;
+                }
+            }
+                        
+            Console.WriteLine($"'{str.Replace(' ', '*')}'");
+        }
+    }
+}
