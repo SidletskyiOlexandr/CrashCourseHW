70a81f5d27aa65432524f13a272b89641c30c470
Olexandr Sidletskyi <scasha2000@gmail.com>
2021-09-22 15:36:27 +0300

    Implement Task7

---
+++ Task7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "SoMe TexT with LoWeR aNd UPPer lettER.";
+            int numLowerLetter=0, numUpperLetter=0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsUpper(str[i]) == true)
+                    numUpperLetter++;
+                else
+                    numLowerLetter++;
+            }
+
+            Console.WriteLine($"Lower: {numLowerLetter*100.0/str.Length}% " +
+                $"Upper: {numUpperLetter * 100.0 / str.Length}% ");
+        }
+    }
+}
