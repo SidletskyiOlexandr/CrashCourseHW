c1ad6fdf3d34324c804374623b1d98ec84615ec2
Olexandr Sidletskyi <scasha2000@gmail.com>
2021-09-22 13:33:22 +0300

    Implement Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter some string: ");
+            string str = Console.ReadLine();
+
+            for (int i = str.IndexOf('.'); i < str.LastIndexOf('.'); i++)
+            {
+                if(str[i]==' ')
+                {
+                    str = str.Remove(i, 1);
+                }
+            }
+            Console.WriteLine(str);
+        }
+    }
+}
