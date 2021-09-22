32294e59e76b477920e745f3a91a9125311450e0
Olexandr Sidletskyi <scasha2000@gmail.com>
2021-09-22 14:26:14 +0300

    Implement Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "My name is Olexandr";
+            int wordIndex = 2;
+            string[] arrStr = str.Split(' ');
+            Console.WriteLine($"{arrStr[wordIndex-1][0]}");
+        }
+    }
+}
