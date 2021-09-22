ae31faee16adc05bd3e6912d0a0fcdb91db61a0f
Olexandr Sidletskyi <scasha2000@gmail.com>
2021-09-22 14:11:17 +0300

    Implement Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static public bool IsPalindrome(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            
+            Array.Reverse(charArray);
+
+            string res = string.Join("", charArray);
+            
+            if (String.Compare(s, res)!=0)
+                return false;
+            else
+                return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Is 'key' palindrome: {IsPalindrome("key")}");
+            Console.WriteLine($"Is 'level' palindrome: {IsPalindrome("level")}");
+        }
+    }
+}
