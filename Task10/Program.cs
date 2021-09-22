0b1cc2f8663fe80dea05974d510f282ab8221f68
Olexandr Sidletskyi <scasha2000@gmail.com>
2021-09-22 23:05:50 +0300

    Implement Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "This is a Task10 ababab";
+
+            string res = null;
+            int largestLeangth = 0;
+
+            string[] arr = str.Split(' ');
+
+            foreach (var item in arr)
+            {
+                if(item.Length >= largestLeangth)
+                {
+                    largestLeangth = item.Length;
+                }
+            }
+            foreach (var item in arr)
+            {
+                if (item.Length == largestLeangth)
+                {
+                    res += item.Replace('a', 'b') + " ";
+                }
+                else
+                    res += item + " ";
+            }
+            res = res.Trim();
+            Console.WriteLine($"'{res}'");
+        }
+    }
+}
---
+++ Task8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] arr = { { 'h', 'r', 'k' }, { 'w', 'q', 'e' }, { 'a', 'q', 'y' } };
+            string enteredWord = "key";
+            
+            int rows = arr.GetUpperBound(0) + 1;
+            int columns = arr.Length / rows;
+            char a=' ';
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if(arr[i,j]==enteredWord[i])
+                    {
+                        a = arr[i, j];
+                        Console.WriteLine(a);
+                    }
+                }
+            }
+            
+        }
+    }
+}
