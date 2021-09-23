20c34ca6525d3337f08e15eba9978949ac2e5c8b
Olexandr Sidletskyi <scasha2000@gmail.com>
2021-09-23 12:13:42 +0300

    Implement Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Remove all words from the string which have less than five characters.";
+            string res = null;
+            
+            string[] arr = str.Split();
+
+            foreach (var item in arr)
+            {
+                if (item.Length>=5)
+                {
+                    res += item + ' '; 
+                }
+            }
+            res = res.Trim();
+
+            Console.WriteLine($"'{res}'");
+        }
+    }
+}
