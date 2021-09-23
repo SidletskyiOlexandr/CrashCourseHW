0bae30a51d77b524204918b2425f060668bbca7e
Olexandr Sidletskyi <scasha2000@gmail.com>
2021-09-23 11:30:40 +0300

    Implemrnt Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "a 10,2 3 v 5,3 A 1 2", intStr = null, floatStr = null, otherStr = null;
+            string[] arr = str.Split();
+
+            foreach (var item in arr)
+            {
+                if (int.TryParse(item, out int a))
+                    intStr += item;
+                
+                else if(float.TryParse(item, out float b))
+                    floatStr += item;
+
+                else
+                    otherStr += item;
+            }
+            
+            Console.WriteLine($"int string: {intStr}\nfloat string: {floatStr}\nother string: {otherStr}");
+        }
+    }
+}
